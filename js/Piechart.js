d9e968cca6e05c8900296e69ed001a43fb1ab89c
Meijie Wang <43675987+wmjpillow@users.noreply.github.com>
2019-02-27 23:35:06 -0500

    Update Piechart.js

--- js/Piechart.js
+++ js/Piechart.js
@@ -7,64 +7,53 @@ function Piechart(canvas){
           var newNumber = Math.floor(Math.random() * 10) + 3;
           data.push(newNumber);
         }
-
-    var radius = Math.min(width,height)/2;
-
-    var arc = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0)
-
-
-    var pie = d3.pie()
-        .sort(null)
-        .value(function(d) { return d; });
-
-
-    d3.select("body").append("svg")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var g=svg.selectAll(".arc")
-          .data(pie(data))
-        .enter().append("g")
-          .attr("class", "arc")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-      g.append("path")
-          .attr("d", arc)
-          .style("fill","white");
-
-    //random 2 areas and draw two points
-        //random two areas
-        var points=[];
-        var datapoints=[];
-        var pt1=Math.floor(Math.random()*10);
-        var pt2=Math.floor(Math.random()*10);
-        while(pt1==pt2){
-            pt2=Math.floor(Math.random()*10);
-        }
-        //sort 2 points in order
-        if (pt1<pt2){
-            points.push(pt1);
-            points.push(pt2);
-            datapoints.push(data[pt1]);
-            datapoints.push(data[pt2]);
-        }
-        else{
-            points.push(pt2);
-            points.push(pt1);
-            datapoints.push(data[pt2]);
-            datapoints.push(data[pt1]);
-        }
+          var arc = d3.arc().outerRadius(200).innerRadius(0);
+          var pointArc = d3.arc().outerRadius(200).innerRadius(0);
+          var pie = d3.pie()
+              .sort(null)
+              .value(function(d) {return d;});
+          var pChart = svg.selectAll(".arc")
+              .data(pie(data)).enter().append("g")
+              .attr("transform", "translate(" + 220 +","+ 300 +")")
+              .attr("class", "arc");
+          pChart.append("path")
+              .attr("d", arc)
+              .style("fill", "white");
+          // add the dot to two of the rectangles
+          var points = [];
+          for(i = 0; i < 2; i++){
+              var pt = Math.floor(Math.random() * 10);
+              if(points.includes(pt)){
+                  i--;
+              }
+              else{
+                  points.push(pt);
+              }
+          }
+          pChart.append("circle")
+              .attr("transform", function(d){
+                  if(points.includes(d.index)){
+                      return "translate(" + pointArc.centroid(d) + ")";
+                  }})
+              .attr("fill", function(d){
+                  if(points.includes(d.index)){
+                      return "white";
+                  }
+              })
+              .attr("r", function(d){
+                  if(points.includes(d.index)){
+                      return 2;
+                  }    
+              })
+              .attr("fill","black");
 
 
-g.selectAll("circle")
-         .data(pie(datapoints))
-         .enter()
-          .append("circle")
-          .attr("transform", function(d) {return "translate(" + arc.centroid(d)  + ")"})
-          .style("fill","black")
-          .attr("r",3);
 
+
+           console.log(data);
+        //   console.log(pie(data));
+        // //  console.log(datapoints);
+           console.log(points);
 
       var ratio= data[points[0]]/data[points[1]];
       var ratio_s= data[points[1]]/data[points[0]];
